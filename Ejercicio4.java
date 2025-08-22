21bb0c73c7e041a00cd4bd65e91958b7a9eabd4e
Mariana González Muciño <mariana.gmucino7@gmail.com>
2025-08-22 14:48:57 -0600

    Se arregla el código

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,17 +1,21 @@
-package com.generation;
+//Se elimina el package
 
-public class Codigo4 {
+import java.util.Scanner;
 
-    Scanner s = new Scanner();
+public class Ejercicio4 { //Se modifica el nombre de la clase
+
+ public static void main(String[] args) { //Se agrega el método main
+	
     
     System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    String j1 = s.nextLine();
+    Scanner s1 = new Scanner(System.in); //Se mueve el scanner y se modifica el s
+	String j1 = s1.nextLine();
     
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    Scanner s2 = new Scanner();
-    String j2 = s.nextLine();
+    System.out.print("Turno del jugador 2 (introduzca piedra, papel o tijeras): "); //Se modifica el número de jugador
+    Scanner s2 = new Scanner(System.in);
+    String j2 = s2.nextLine(); //Se modifica el s
     
-    if (j1 == j2)) {
+    if (j1 == j2) {
       System.out.println("Empate");
     } else {
       int g = 2;
@@ -24,16 +28,21 @@ public class Codigo4 {
         case "papel":
           if (j2 == "piedra") {
             g = 1;
+          }
           
         case "tijera":
           if (j2.equals("papel")) {
             g = 1;
-          }
+          }// se agrega {
           break;
         default:
       }
       System.out.println("Gana el jugador " + g);
     }
   
-  
-}
+    
+    
+	 }//main
+	 
+	 
+}// Se cierra la clase
